da6c4e8b8e9db7ebc3f17900fa46388d7da395a9
hheeju98 <hheeju98@naver.com>
2024-02-01 15:35:46 +0900

    모달 save시 탭별 데이터 객체로

--- js/myVessel/data/data.js
+++ js/myVessel/data/data.js
@@ -24,3 +24,48 @@ Classification = [
   "American Bureau of Shipping",
   "Bareau Veritas",
 ];
+
+EU_Ship_Type = [
+  "Passenger Ship",
+  "Ro-ro ship",
+  "Container Ship",
+  "Oil tanker",
+  "Chemical tanker",
+  "LNG carrier",
+  "Gas carrier",
+  "Bulk carrier",
+  "General cargo shop",
+];
+
+MRV_Company = [
+  "Eastaway Ship Management.PTE.LTD",
+  "Aniaftis Shipping Co",
+  "DORIKO Limited",
+  "HYNDAI GLOVIS Co Ltd",
+  "Sea Pionner Denizcilik ve Ticaret Ltd.Sti.",
+];
+
+MRV_Method = [
+  "Method A : BDN and periodic sticktakes of fuel tanks",
+  "Method B : Bunker fuel tank monitoring on-board",
+];
+
+IMO_Ship_type = [
+  "BulkCarrier",
+  "GasCarrier",
+  "Tanker",
+  "ContainerShip",
+  "GeneralCargoShip",
+  "RefrigeratedCargoCarrier",
+  "CombinationCarrier",
+  "PassengerShip",
+  "RoRoCargoShipVehicleCarrier",
+];
+
+DCS_Method = [
+  "Method using BDNs",
+  "Method using flow meters matchMedia",
+  "Method using bunker fuel oil tank monitating",
+];
+
+Trial_Puropse = ["EEOI", "EEPI", "cIDIST", "cdDIST"];
--- js/myVessel/modal/index.js
+++ js/myVessel/modal/index.js
@@ -1,23 +1,7 @@
 let originData;
 const shipParticularTab = document.getElementById("shipParticular");
-const IMO = document.querySelector(".IMO-input");
-const shipName = document.querySelector(".ship-name-input");
-const GT = document.querySelector(".gt-input");
-const NT = document.getElementById("nt");
-const DWT = document.querySelector(".dwt-input");
-const EEDI = document.querySelector(".EEDI-input");
-const iceClass = document.getElementById("iceClass");
-const propulsion = document.querySelector(".propulsion-input");
-const auxiliary = document.querySelector(".Auxiliary-input");
-const SFOC = document.querySelector(".SFOC-input");
-const flag = document.getElementById("flag");
-const registry = document.getElementById("portOfRegistry");
-const shipOwner = document.getElementById("shipOwner");
-const classification = document.getElementById("classification");
-const officialNO = document.getElementById("officialNO");
-const signalLetter = document.getElementById("signalLetter");
-const remark = document.querySelector(".remark-input"); //탭마다 값 들고오기
 
+let currentTab = document.getElementById("shipParticular");
 function setModal() {
   const mrvTab = document.getElementById("mrv");
   const dcsTab = document.getElementById("dcs");
@@ -31,16 +15,23 @@ function setModal() {
 }
 
 function setData(e) {
+  console.log(e.data);
   popupWithScrollView.show();
   setModal();
-  shipParticularTab.style.display = "block"; // 안에서 전역말고
+  shipParticularTab.style.display = "block";
   shipParticularBtn.classList.add("active-modal-btn");
-  IMO.value = e.IMO_NO;
+  // IMO.value = e.data.IMO_NO;
   originData = e;
   setSelectBox("#iceClass", Ice_Class);
   setSelectBox("#flag", Flag);
   setSelectBox("#portOfRegistry", Port_of_Registry);
   setSelectBox("#classification", Classification);
+  setSelectBox("#EUShipType", EU_Ship_Type);
+  setSelectBox("#MRVcompany", MRV_Company);
+  setSelectBox("#mrvMethod", MRV_Method);
+  setSelectBox("#IMOshipType", IMO_Ship_type);
+  setSelectBox("#DCSmethod", DCS_Method);
+  setSelectBox("#trialPurpose", Trial_Puropse);
 }
 
 function showModalTab(e, type) {
@@ -51,29 +42,84 @@ function showModalTab(e, type) {
   setModal();
   const tab = document.querySelector(`#${type}`);
   tab.style.display = "block";
+  currentTab = tab;
+  console.log(currentTab);
   e.target.classList.add("active-modal-btn");
 }
 
 function saveData() {
-  // 함수 호출-> 무슨 탭인지 전달 함수에 -> 하수에 전달받은 타입에 따른 객체-> 리턴
-  const changedData = {};
-  changedData.IMO_NO = IMO.value;
-  changedData.Ship_Name = shipName.value;
-  changedData.GT = GT.value;
-  changedData.NT = NT.value;
-  changedData.DWT = DWT.value;
-  changedData.EEDI = EEDI.value;
-  changedData.Ice_Class = iceClass.textContent;
-  changedData.PROPULSION = propulsion.value;
-  changedData.AUXILIARY = auxiliary.value;
-  changedData.SFOC = SFOC.value;
-  changedData.Flag = flag.textContent;
-  changedData.REGISTRY = registry.textContent;
-  changedData.SHIPOWNER = shipOwner.value;
-  changedData.Classification = classification.textContent;
-  changedData.OfficialNO = officialNO.value;
-  changedData.SignalLetter = signalLetter.value;
-  changedData.remark = remark.value;
-  console.log(changedData);
+  const IMO = document.querySelector(".IMO-input");
+  const shipName = document.querySelector(".ship-name-input");
+  const GT = document.querySelector(".gt-input");
+  const NT = document.getElementById("nt");
+  const DWT = document.querySelector(".dwt-input");
+  const EEDI = document.querySelector(".EEDI-input");
+  const iceClass = document.getElementById("iceClass");
+  const propulsion = document.querySelector(".propulsion-input");
+  const auxiliary = document.querySelector(".Auxiliary-input");
+  const SFOC = document.querySelector(".SFOC-input");
+  const flag = document.getElementById("flag");
+  const registry = document.getElementById("portOfRegistry");
+  const shipOwner = document.getElementById("shipOwner");
+  const classification = document.getElementById("classification");
+  const officialNO = document.getElementById("officialNO");
+  const signalLetter = document.getElementById("signalLetter");
+  const remark = document.querySelector(".remark-input");
+  const EUShipType = document.getElementById("EUShipType");
+  const MRVcompany = document.getElementById("MRVcompany");
+  const mrvMethod = document.getElementById("mrvMethod");
+  const IMO_ShipType = document.getElementById("IMOshipType");
+  const DCS_Method = document.getElementById("DCSmethod");
+  const Year_Of_Delivery = document.getElementById("yearOfDelivery");
+  const EEXI = document.getElementById("EEXI");
+  const FI = document.getElementById("fi");
+  const FM = document.getElementById("fm");
+  const FC = document.getElementById("fc");
+  const FI_VSE = document.getElementById("fiVse");
+  const Trial_Purpose = document.getElementById("trialPurpose");
+
+  const shipParticularChangedData = {};
+  const mrvChangedData = {};
+  const dcsChangedData = {};
+  const ciiChangedData = {};
+
+  if (currentTab.id === "shipParticular") {
+    shipParticularChangedData.IMO_NO = IMO.value;
+    shipParticularChangedData.Ship_Name = shipName.value;
+    shipParticularChangedData.GT = GT.value;
+    shipParticularChangedData.NT = NT.value;
+    shipParticularChangedData.DWT = DWT.value;
+    shipParticularChangedData.EEDI = EEDI.value;
+    shipParticularChangedData.Ice_Class = iceClass.textContent;
+    shipParticularChangedData.PROPULSION = propulsion.value;
+    shipParticularChangedData.AUXILIARY = auxiliary.value;
+    shipParticularChangedData.SFOC = SFOC.value;
+    shipParticularChangedData.Flag = flag.textContent;
+    shipParticularChangedData.REGISTRY = registry.textContent;
+    shipParticularChangedData.SHIPOWNER = shipOwner.value;
+    shipParticularChangedData.Classification = classification.textContent;
+    shipParticularChangedData.OfficialNO = officialNO.value;
+    shipParticularChangedData.SignalLetter = signalLetter.value;
+    shipParticularChangedData.remark = remark.value;
+    console.log(shipParticularChangedData);
+  } else if (currentTab.id === "mrv") {
+    mrvChangedData.EU_ShipType = EUShipType.textContent;
+    mrvChangedData.MRV_Company = MRVcompany.textContent;
+    mrvChangedData.MRV_Method = mrvMethod.textContents;
+    console.log(mrvChangedData);
+  } else if (currentTab.id === "dcs") {
+    dcsChangedData.IMO_ShipType = IMO_ShipType.textContent;
+    dcsChangedData.DCS_Method = DCS_Method.textContent;
+    dcsChangedData.Year_Of_Delivery = Year_Of_Delivery.value;
+    dcsChangedData.EEXI = EEXI.value;
+    console.log(dcsChangedData);
+  } else if (currentTab.id === "cii") {
+    ciiChangedData.FI = FI.value;
+    ciiChangedData.FM = FM.value;
+    ciiChangedData.FC = FC.value;
+    ciiChangedData.FI_VSE = FI_VSE.value;
+    ciiChangedData.Trial_Purpose = Trial_Purpose.textContent;
+    console.log(ciiChangedData);
+  }
   popupWithScrollView.hide();
 }
